1b3d7a11619e40a98c67925c93374ccae4af1bad
Matthew Waskin <matthew@waskin.com>
2023-06-26 15:38:19 -0400

    assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'assignment3-directives';
+  displayDetails: boolean = false;
+  buttonText: string = this.displayDetails ? 'Hide Details' : 'Display Details';
+  // clickCount: number = 0;
+  clickLog = [];
+
+  onButtonClick() {
+    // this.clickCount += 1;
+    this.displayDetails = !this.displayDetails;
+    this.clickLog.push(new Date());
+  }
 }
